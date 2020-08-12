e00d8a224e971db17773b952eeccfb60898fd142
Alexis Barba <alexbarba.2278@gmail.com>
2020-08-11 22:31:36 -0500

    Anadido footer

---
+++ layouts/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex items-center bg-gray-200 h-16 justify-center">
+    <div class="">Alexis Barba © - 2020</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,40 +1,45 @@
 <template>
-  <div class="container mx-auto main-layout">
-    <div class="divide-y-2 divide-black divide-opacity-25">
-      <div>
-        <HeaderMain
-          :title="title"
-          :description="description"
-          :contact="contact"
-        />
-        <div class="">
-          <n-link
-            to="/"
-            class="text-blue-800 underline text-xl hover:text-teal-800 m-2"
-            >Portfolio</n-link
-          >
-          <p class="text-blue-800 inline">|</p>
-          <n-link
-            to="/about"
-            class="text-blue-800 underline text-xl hover:text-teal-800 m-2"
-            >About</n-link
-          >
+  <div>
+    <div class="container mx-auto main-layout">
+      <div class="divide-y-2 divide-black divide-opacity-25">
+        <div>
+          <HeaderMain
+            :title="title"
+            :description="description"
+            :contact="contact"
+          />
+          <div class="">
+            <n-link
+              to="/"
+              class="text-blue-800 underline text-xl hover:text-teal-800 m-2"
+              >Portfolio</n-link
+            >
+            <p class="text-blue-800 inline">|</p>
+            <n-link
+              to="/about"
+              class="text-blue-800 underline text-xl hover:text-teal-800 m-2"
+              >About</n-link
+            >
+          </div>
+        </div>
+        <div class="m-4">
+          <slot class="m-8"></slot>
         </div>
       </div>
-      <div class="m-4">
-        <slot class="m-8"></slot>
-      </div>
     </div>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import HeaderMain from './HeaderMain';
+import Footer from './Footer';
 
 export default {
   name: 'MainLayout',
   components: {
     HeaderMain,
+    Footer,
   },
   data: () => {
     return {
