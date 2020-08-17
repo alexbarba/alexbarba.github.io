7de35b9f674fae09f75137382e486aa30aea8ca6
Alexis Barba <alexbarba.2278@gmail.com>
2020-08-16 20:29:27 -0500

    Quitando archivos binarios del repositorio y cambiando enlaces a imgur

--- pages/index.vue
+++ pages/index.vue
@@ -119,8 +119,7 @@ export default {
             'Calculadora binaria hecha con Vainilla JS y CSS, usando Regex, a prueba de errores, para el reto "10 Days of Javascript" de Hackerrank',
           labels: ['Calculadora', 'Javascript', 'CSS'],
           url: 'https://alexbarba.github.io/BinaryCalculator/',
-          img:
-            'https://raw.githubusercontent.com/alexbarba/me/master/assets/binarycalculator.png',
+          img: 'https://i.imgur.com/tKNvJ7t.png',
         },
       ],
     };
